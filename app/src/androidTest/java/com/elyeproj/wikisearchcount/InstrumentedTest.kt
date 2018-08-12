90be10b763814af000408f246816660d2fe181e5
elye <elye.project@gmail.com>
2018-08-12 22:17:32 +1000

    Add the okreplay library to the instrumental test

--- app/src/androidTest/java/com/elyeproj/wikisearchcount/InstrumentedTest.kt
+++ app/src/androidTest/java/com/elyeproj/wikisearchcount/InstrumentedTest.kt
@@ -1,13 +1,21 @@
 package com.elyeproj.wikisearchcount
 
+import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.IdlingRegistry
 import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
+import android.support.test.rule.GrantPermissionRule
 import android.support.test.runner.AndroidJUnit4
+import okreplay.AndroidTapeRoot
+import okreplay.OkReplay
+import okreplay.OkReplayConfig
+import okreplay.OkReplayRuleChain
+import okreplay.TapeMode
 import org.junit.Before
+import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,9 +27,26 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class InstrumentedTest {
+
+    private val okReplayConfig = OkReplayConfig.Builder()
+            .tapeRoot(AndroidTapeRoot(InstrumentationRegistry.getContext(), javaClass))
+            .defaultMode(TapeMode.READ_WRITE) // or TapeMode.READ_ONLY
+            .sslEnabled(true)
+            .interceptor(WikiApiService.okReplayInterceptor)
+            .build()
+
+    companion object {
+        @ClassRule
+        @JvmField
+        val grantExternalStoragePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
     @get:Rule
     private val activityRule = ActivityTestRule(MainActivity::class.java, false, false)
     private val fetchingIdlingResource = FetchingIdlingResource()
+
+    @get:Rule
+    val testRule = OkReplayRuleChain(okReplayConfig, activityRule).get()
 
     @Before
     fun setup() {
@@ -31,13 +56,13 @@ class InstrumentedTest {
     }
 
     @Test
-    @Throws(Exception::class)
+    @OkReplay(tape = "instrumental launch and search", mode = TapeMode.READ_WRITE)
     fun launchAndSearch() {
         Espresso.onView(ViewMatchers.withId(R.id.edit_search))
                 .perform(ViewActions.replaceText("Trump"))
         Espresso.onView(ViewMatchers.withId(R.id.btn_search))
                 .perform(ViewActions.click())
         Espresso.onView(ViewMatchers.withId(R.id.txt_search_result))
-                .check(ViewAssertions.matches(ViewMatchers.withText("22911 result found")))
+                .check(ViewAssertions.matches(ViewMatchers.withText("22908 result found")))
     }
 }
--- app/src/main/java/com/elyeproj/wikisearchcount/WikiApiService.kt
+++ app/src/main/java/com/elyeproj/wikisearchcount/WikiApiService.kt
@@ -1,14 +1,15 @@
 package com.elyeproj.wikisearchcount
 
 import io.reactivex.Observable
-import retrofit2.http.GET
+import okhttp3.OkHttpClient
+import okreplay.OkReplayInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WikiApiService {
-
     @GET("api.php")
     fun hitCountCheck(@Query("action") action: String,
                        @Query("format") format: String,
@@ -16,12 +17,17 @@ interface WikiApiService {
                        @Query("srsearch") srsearch: String): Observable<Model.Result>
 
     companion object {
+        val okReplayInterceptor = OkReplayInterceptor()
+
         fun create(): WikiApiService {
+            val builder = OkHttpClient.Builder()
+            builder.addInterceptor(okReplayInterceptor)
 
             val retrofit = Retrofit.Builder()
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .baseUrl("https://en.wikipedia.org/w/")
+                    .client(builder.build())
                     .build()
 
             return retrofit.create(WikiApiService::class.java)
